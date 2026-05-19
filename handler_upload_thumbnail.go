d3867e9bad110aba0fd291df32b58683f52879f2
kaiserkimguin <kpkabasele@gmail.com>
2026-05-19 20:38:47 +0200

    260519 thumbnail storage implemented and tested. database initialized.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,52 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, 500, "issues", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse from file", err)
+		return
+	}
+	mT := header.Header.Get("Content-Type")
+
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read file", err)
+		return
+	}
+
+	// get video metadata
+	v, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Video not found", err)
+		return
+	}
+	if v.ID == uuid.Nil {
+		respondWithError(w, http.StatusNotFound, "Video does not exist", err)
+		return
+	}
+	if v.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+		return
+	}
+
+	tURL := fmt.Sprintf("data:%s;base64,%s", mT, base64.StdEncoding.EncodeToString(fileData))
+
+	v.ThumbnailURL = &tURL
+
+	err = cfg.db.UpdateVideo(v)
+	if err != nil {
+		respondWithError(w, 500, "unable to update video data", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, v)
 }
